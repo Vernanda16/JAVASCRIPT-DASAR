77d10a3826a847353db46771344ca0c2474894ad
vernanda16 <vernanda515@gmail.com>
2020-01-04 22:36:37 +0700

    menambahkan file TipeDataNumber.js

---
+++ TipeDataNumber.js
@@ -0,0 +1,9 @@
+//pada javascript sebuah tipe data tidak perlu di tuliskan, hal ini dikarenakan javascript bersifat dynamic typing sehingga akan mengenali jenis tipe data tanpa ditulis terlebih dahulu
+
+var bulat = '16';
+var desimal = 3.5;
+var negatif = -10;
+
+console.log(bulat);
+console.log(desimal);
+console.log(negatif);
