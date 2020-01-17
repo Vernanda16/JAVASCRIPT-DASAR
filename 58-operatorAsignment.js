2b80e31bd7429ae3b9ae7851c86350ff2c7845fc
vernanda16 <vernanda515@gmail.com>
2020-01-17 15:23:48 +0700

    menambah materi ke 58

---
+++ 58-operatorAsignment.js
@@ -0,0 +1,17 @@
+//opeartor asignment atau sama dengan, berguna untuk mengisi nilai variable
+
+var a;
+var b;
+var c;
+
+a = 10;
+b = 9;
+c = 8;
+
+console.log(a, b, c);
+
+//kita juga bisa mengubah nilai variable
+a = 6;
+b = 5;
+c = 4;
+console.log(a, b, c)
